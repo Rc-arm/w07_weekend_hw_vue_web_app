899d19604aaf9f8a3ead9c57eff44db20d52e885
Rob Armitage <robert.c.armitage@gmail.com>
2019-09-16 01:23:45 +0100

    change api

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
     <h1>Articles List App</h1>
+    <div class="main-container">
       <articles-list :articles="articles"></articles-list>
       <article-detail :article="selectedArticle"></article-detail>
+    </div>
   </div>
 </template>
 
 <script>
-import ArticlesList from './components/ArticlesList'
-import ArticleDetail from './components/ArticleDetail'
 import {eventBus} from './main'
+import ArticleDetail from './components/ArticleDetail'
+import ArticlesList from './components/ArticlesList'
 
 export default {
   data(){
@@ -23,7 +25,8 @@ export default {
     "article-detail": ArticleDetail
   },
   mounted(){
-    fetch('https://www.reddit.com/r/javascript.json')
+    // fetch('https://www.reddit.com/r/javascript.json')
+    fetch('https://restcountries.eu/rest/v2/all')
     .then(result => result.json())
     .then(articles => this.articles = articles)
 
